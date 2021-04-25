03b83b8ae8dbc9de29919ec359ee7d29c5aada8e
Calebe-Xavier-Developer <57916053+Calebe-Xavier-Developer@users.noreply.github.com>
2021-04-25 09:35:57 -0300

    Recebendo a Home de forma estática

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,47 @@
-export default function Home() {
+/*SPA
+useEffect(() => {//primeiro parametro é o que quer executar
+    fetch('http://localhost:3434/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, []) //o segundo é quando
+*/
+/* SSR
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3434/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+}
+*/
+//SSG Gera uma versão estatica que é servida para varias pessoas, sem requisição nova
+
+//import { useEffect } from "react"; Dispara algo sempre que algo mudar(efeitos colarerais);
+
+export default function Home(props) {
+  console.log(props.episodes);
+
   return (
     <>
       <h1>Hello World! sera ?</h1>
+      <div>
+        <p>{JSON.stringify(props.episodes)}</p>
+      </div>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3434/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
